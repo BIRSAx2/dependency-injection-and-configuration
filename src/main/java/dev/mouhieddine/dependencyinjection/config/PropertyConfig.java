ae5b67877fa3a4a8e0ee239f22e642bf79519236
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-25 16:33:35 +0100

    Spring Boot Profile Properties

--- src/main/java/dev/mouhieddine/dependencyinjection/config/PropertyConfig.java
+++ src/main/java/dev/mouhieddine/dependencyinjection/config/PropertyConfig.java
@@ -16,39 +16,39 @@ import org.springframework.core.env.Environment;
 public class PropertyConfig {
 
 
-    @Value("${dev.username}")
-    String user;
-    @Value("${dev.password}")
-    String password;
-    @Value("${dev.dburl}")
-    String url;
+  @Value("${dev.username}")
+  String user;
+  @Value("${dev.password}")
+  String password;
+  @Value("${dev.dburl}")
+  String url;
 
-    @Value("${dev.jms.username}")
-    String jmsUser;
-    @Value("${dev.jms.password}")
-    String jmsPassword;
-    @Value("${dev.jms.dburl}")
-    String jmsUrl;
+  @Value("${dev.jms.username}")
+  String jmsUser;
+  @Value("${dev.jms.password}")
+  String jmsPassword;
+  @Value("${dev.jms.dburl}")
+  String jmsUrl;
 
-    @Bean
-    public FakeDataSource fakeDataSource() {
-        FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setPassword(password);
-        fakeDataSource.setUrl(url);
-        fakeDataSource.setUser(user);
-        return fakeDataSource;
-
-
-    }
-
-    @Bean
-    public FakeJmsBroker fakeJmsBroker() {
-        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
-        fakeJmsBroker.setPassword(jmsPassword);
-        fakeJmsBroker.setUrl(jmsUrl);
-        fakeJmsBroker.setUser(jmsUser);
-        return fakeJmsBroker;
+  @Bean
+  public FakeDataSource fakeDataSource() {
+    FakeDataSource fakeDataSource = new FakeDataSource();
+    fakeDataSource.setPassword(password);
+    fakeDataSource.setUrl(url);
+    fakeDataSource.setUser(user);
+    return fakeDataSource;
 
 
-    }
+  }
+
+  @Bean
+  public FakeJmsBroker fakeJmsBroker() {
+    FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+    fakeJmsBroker.setPassword(jmsPassword);
+    fakeJmsBroker.setUrl(jmsUrl);
+    fakeJmsBroker.setUser(jmsUser);
+    return fakeJmsBroker;
+
+
+  }
 }
